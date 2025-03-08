3799c7e30af97a9051ea8b70d20b734854338d12
nmkhoi3006 <khoi30062005@gmail.com>
2025-03-08 22:58:37 +0700

    done

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
+const nameGirl = 'Manh bịp';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Happy Women\'s Day';
+const titleCard = 'Tặng Manh bịp';
+const contentCard = "Chúc Manh bịp luôn xin đẹp và bớt bố láo lại nhaaâ";
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
