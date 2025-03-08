f9a00278326561e6677dd8c348dd84b904045897
nmkhoi3006 <khoi30062005@gmail.com>
2025-03-08 23:44:37 +0700

    f

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Manh bịp';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Happy Women\'s Day';
 const titleCard = 'Tặng Manh bịp';
-const contentCard = "Chúc Manh bịp luôn xin đẹp và bớt bố láo lại nhaaâ";
+const contentCard = "Chúc Manh bịp luôn xinh đẹp và bớt bố láo lại nhaaaaaa";
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
